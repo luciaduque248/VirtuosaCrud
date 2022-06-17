a79dc4360c50f3509f43028313f7f4e26636a03b
luciaduque248 <luciaduque248@gmail.com>
2022-06-17 18:40:00 -0500

    routes of maquillaje was modified

--- src/pages/maquillaje/Makeup.js
+++ src/pages/maquillaje/Makeup.js
@@ -15,28 +15,28 @@ function Makeup() {
 
                 <div className='atajos-makeup'>
                     <div className='atajo-1'>
-                        <Link to='/ojos'  className='letra-atajos-makeup'>
+                        <Link to='/VirtuosaCrud/ojos'  className='letra-atajos-makeup'>
                             <img src='https://cdn-icons-png.flaticon.com/512/82/82832.png' alt="atajos" className='atajo-ojos' />
                             Ojos
                         </Link>
                     </div>
 
                     <div className='atajo-1'>
-                        <Link to='/labios' className='letra-atajos-makeup'>
+                        <Link to='/VirtuosaCrud/labios' className='letra-atajos-makeup'>
                             <img src='https://cdn-icons-png.flaticon.com/512/1024/1024505.png' alt="atajos" className='atajo-ojos' />
                             Labios
                         </Link>
                     </div>
 
                     <div className='atajo-1'>
-                        <Link to='/piel' className='letra-atajos-makeup'>
+                        <Link to='/VirtuosaCrud/piel' className='letra-atajos-makeup'>
                             <img src='https://cdn-icons-png.flaticon.com/512/1207/1207069.png' alt="atajos" className='atajo-ojos' />
                             Piel
                         </Link>
                     </div>
 
                     <div className='atajo-1'>
-                        <Link to='/rostro' className='letra-atajos-makeup'>
+                        <Link to='/VirtuosaCrud/rostro' className='letra-atajos-makeup'>
                             <img src='https://cdn-icons-png.flaticon.com/512/5731/5731856.png' alt="atajos" className='atajo-ojos' />
                             Rostro
                         </Link>
